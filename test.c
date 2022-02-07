6ab3335a8c915e125a6a1f4290b3db7f8f3baf4e
Govindmegan <99072615+Govindmegan@users.noreply.github.com>
2022-02-07 20:32:26 +0530

    fourth user commit

--- test.c
+++ test.c
@@ -7,7 +7,7 @@ for(int i=0;i<100;i++)
     printf("KEEP GOING ,SUCCESS FOLLOWS U\n");
     
 }
-    for(int k1=0;k1<100;k1++)
+    for(int k3=0;k3<100;k3++)
     {
         printf("NEVER STOP\n");
     }

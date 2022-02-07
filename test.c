e36fc5f523a56772a22c2d5b354780821e6fd666
unknown <Govind>
2022-02-07 20:39:11 +0530

    first commit

--- test.c
+++ test.c
@@ -4,12 +4,8 @@ int main()
 
 for(int i=0;i<100;i++)
 {
-    printf("KEEP GOING ,SUCCESS FOLLOWS U\n");
+    printf("INFOTAINMENT\n");
     
 }
-    for(int kt=0;kt<100;kt++)
-    {
-        printf("NEVER STOP\n");
-    }
 
 }

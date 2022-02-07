2c4000665447df6885c4de906ec86d5e83300a8c
unknown <Govind>
2022-02-07 20:31:32 +0530

    Third Commit

--- test.c
+++ test.c
@@ -7,7 +7,7 @@ for(int i=0;i<100;i++)
     printf("KEEP GOING ,SUCCESS FOLLOWS U\n");
     
 }
-    for(int k1=0;k1<100;k1++)
+    for(int k2=0;k2<100;k2++)
     {
         printf("NEVER STOP\n");
     }
